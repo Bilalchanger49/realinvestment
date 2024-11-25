990d2a6c38fd6ac3856a210172ce3da20471efe4
imhamza-1 <hamzailyas1282@gmail.com>
2024-11-25 12:43:16 +0500

    <<hamza>> investor page edited

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -12,6 +12,8 @@
     <!-- Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href="assets/css/vendor.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/responsive.css">
