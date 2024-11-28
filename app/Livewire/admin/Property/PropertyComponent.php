27cc7aabb702c881384631719f6b5e2295d72a25
Bilalchanger49 <bilalchanger49@gmail.com>
2024-11-28 22:09:37 +0500

    <<Bilal>>  property update and delete

--- app/Livewire/admin/Property/PropertyComponent.php
+++ app/Livewire/admin/Property/PropertyComponent.php
@@ -7,6 +7,27 @@ use Livewire\Component;
 
 class PropertyComponent extends Component
 {
+    public $deleteId;
+
+    protected $listeners = ['confirmDelete'];
+
+    public function confirmDelete($id)
+    {
+        $this->deleteId = $id;
+    }
+    public function deleteProperty()
+    {
+        $property = Property::find($this->deleteId);
+
+        if ($property) {
+            $property->delete();
+            session()->flash('message', 'Property deleted successfully.');
+        } else {
+            session()->flash('error', 'Property not found.');
+        }
+
+        $this->deleteId = null; // Reset deleteId
+    }
     public function render()
     {
         $properties = Property::all();
--- resources/views/livewire/admin/property/index.blade.php
+++ resources/views/livewire/admin/property/index.blade.php
@@ -12,11 +12,12 @@
                         <tr>
                             <th>Name</th>
                             <th>Description</th>
-                            <th>Registeration no</th>
+                            <th>Reg no</th>
                             <th>Address</th>
                             <th>Price</th>
                             <th>Rent</th>
                             <th>Share Price</th>
+                            <th>Total Shares</th>
                             <th>Remaining Share</th>
                             <th>Image</th>
                             <th>Actions</th>
@@ -32,20 +33,48 @@
                                 <td>{{ $property->property_price }}</td>
                                 <td>{{ $property->property_rent }}</td>
                                 <td>{{ $property->property_share_price }}</td>
+                                <td>{{ $property->property_total_shares }}</td>
                                 <td>{{ $property->property_remaining_shares }}</td>
                                 <td><img src="{{ asset('storage/' . $property->property_image) }}" alt="Course Thumbnail"
                                          width="100"></td>
                                 <td>
-{{--                                    <button class="btn btn-danger">--}}
-{{--                                        <a  href="{{route('courses.destroy', $property->id)}}" class="text-decoration-none text-light">--}}
-{{--                                            <i class="icon-trash"></i> Delete--}}
-{{--                                        </a>--}}
-{{--                                    </button>--}}
+                                    <button class="btn btn-danger">
+                                        <a  href="{{route('admin.property.edit', $property->id)}}" class="text-decoration-none text-light">
+                                            <i class="icon-trash"></i> Edit
+                                        </a>
+                                    </button>
+                                    <button wire:click="confirmDelete({{ $property->id }})" class="btn btn-danger">
+                                            <i class="icon-trash"></i> Delete
+                                    </button>
                                 </td>
                             </tr>
                         @endforeach
                         </tbody>
                     </table>
+                    <!-- Confirmation Modal -->
+                    @if ($deleteId)
+                        <div class="modal fade show d-block" style="background-color: rgba(0, 0, 0, 0.5);" tabindex="-1">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Confirm Deletion</h5>
+                                        <button type="button" wire:click="$set('deleteId', null)" class="btn-close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Are you sure you want to delete this property?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" wire:click="$set('deleteId', null)" class="btn btn-secondary">
+                                            Cancel
+                                        </button>
+                                        <button type="button" wire:click="deleteProperty" class="btn btn-danger">
+                                            Delete
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
