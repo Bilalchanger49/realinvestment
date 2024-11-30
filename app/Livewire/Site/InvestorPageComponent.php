1037369b8bd114fd7ee8d21d66b73e2ab9b82151
Furqan-024 <furqanmusawar0024@gmail.com>
2024-11-30 12:14:40 +0500

    <<Furqan>> Active transactions

--- app/Livewire/Site/InvestorPageComponent.php
+++ app/Livewire/Site/InvestorPageComponent.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire\Site;
 
+use App\Models\Property_investment;
 use Livewire\Component;
 
 class InvestorPageComponent extends Component
 {
     public function render()
     {
-        return view('livewire.site.investorPage' )->extends('layouts.site');
+        if (!empty(auth()->user())) {
+            $user = auth()->user();
+        }else{
+            return redirect('/login');
+        }
+        $propertyInvestments = Property_investment::where('user_id', $user->id)
+            ->with('property')->get();
+//      dd($propertyInvestments[0]->property->property_name);
+        return view('livewire.site.investorPage', compact('propertyInvestments', 'user'))->extends('layouts.site');
     }
 }
