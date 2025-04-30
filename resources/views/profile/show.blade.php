7ccb92b71fe37a0338096786374f192b12096cbf
Bilalchanger49 <bilalchanger49@gmail.com>
2025-04-30 12:07:23 +0500

    <<Bilal>> signature pad fixed

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,48 +1,4 @@
-{{--<x-app-layout>--}}
-{{--    <x-slot name="header">--}}
-{{--        <h2 class="font-semibold text-xl text-gray-800 leading-tight">--}}
-{{--            {{ __('Profile') }}--}}
-{{--        </h2>--}}
-{{--    </x-slot>--}}
 
-{{--    <div>--}}
-{{--        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">--}}
-{{--            @if (Laravel\Fortify\Features::canUpdateProfileInformation())--}}
-{{--                @livewire('profile.update-profile-information-form')--}}
-
-{{--                <x-section-border />--}}
-{{--            @endif--}}
-
-{{--            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))--}}
-{{--                <div class="mt-10 sm:mt-0">--}}
-{{--                    @livewire('profile.update-password-form')--}}
-{{--                </div>--}}
-
-{{--                <x-section-border />--}}
-{{--            @endif--}}
-
-{{--            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())--}}
-{{--                <div class="mt-10 sm:mt-0">--}}
-{{--                    @livewire('profile.two-factor-authentication-form')--}}
-{{--                </div>--}}
-
-{{--                <x-section-border />--}}
-{{--            @endif--}}
-
-{{--            <div class="mt-10 sm:mt-0">--}}
-{{--                @livewire('profile.logout-other-browser-sessions-form')--}}
-{{--            </div>--}}
-
-{{--            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())--}}
-{{--                <x-section-border />--}}
-
-{{--                <div class="mt-10 sm:mt-0">--}}
-{{--                    @livewire('profile.delete-user-form')--}}
-{{--                </div>--}}
-{{--            @endif--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</x-app-layout>--}}
 @extends('layouts.site')
 @section('content')
 
@@ -72,8 +28,6 @@
                 <div class="list-group list-group-flush account-settings-links">
                     <a class="list-group-item list-group-item-action active" data-bs-toggle="list" href="#account-general">General</a>
                     <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#account-change-password">Change Password</a>
-                    <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#account-info">Info</a>
-                    <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#account-notifications">Notifications</a>
                 </div>
             </div>
 
@@ -83,104 +37,16 @@
                     <!-- General Section -->
                     @livewire('profile.update-profile-information-form')
 
-{{--                    <div class="tab-pane fade show active" id="account-general">--}}
-{{--                        <div class="cd-card-body d-flex align-items-center">--}}
-{{--                            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt class="d-block ui-w-80">--}}
-{{--                            <div class="ms-4">--}}
-{{--                                <label class="btn btn-outline-primary">--}}
-{{--                                    Upload new photo--}}
-{{--                                    <input type="file" class="account-settings-fileinput">--}}
-{{--                                </label> &nbsp;--}}
-{{--                                <button type="button" class="btn btn-secondary">Reset</button>--}}
-{{--                                <div class="text-light small mt-1">Allowed JPG, GIF or PNG. Max size of 800K</div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <hr class="border-light m-0">--}}
-{{--                        <div class="cd-card-body">--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label class="form-label">Username</label>--}}
-{{--                                <input type="text" class="form-control" value="nmaxwell">--}}
-{{--                            </div>--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label class="form-label">Name</label>--}}
-{{--                                <input type="text" class="form-control" value="Nelle Maxwell">--}}
-{{--                            </div>--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label class="form-label">E-mail</label>--}}
-{{--                                <input type="text" class="form-control" value="nmaxwell@mail.com">--}}
-{{--                                <div class="alert alert-warning mt-3">--}}
-{{--                                    Your email is not confirmed. Please check your inbox.<br>--}}
-{{--                                    <a href="javascript:void(0)">Resend confirmation</a>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label class="form-label">Company</label>--}}
-{{--                                <input type="text" class="form-control" value="Company Ltd.">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
 
                     <!-- Change Password Section -->
                     @livewire('profile.update-password-form')
-{{--                    <div class="tab-pane fade" id="account-change-password">--}}
-{{--                        <div class="cd-card-body">--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label class="form-label">Current Password</label>--}}
-{{--                                <input type="password" class="form-control">--}}
-{{--                            </div>--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label class="form-label">New Password</label>--}}
-{{--                                <input type="password" class="form-control">--}}
-{{--                            </div>--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label class="form-label">Repeat New Password</label>--}}
-{{--                                <input type="password" class="form-control">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
 
-                    <!-- Info Section -->
-                    <div class="tab-pane fade" id="account-info">
-                        <div class="cd-card-body">
-                            <div class="mb-3">
-                                <label class="form-label">Phone Number</label>
-                                <input type="text" class="form-control" value="+123456789">
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-label">Address</label>
-                                <input type="text" class="form-control" value="123 Main St, City, Country">
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-label">Bio</label>
-                                <textarea class="form-control" rows="3">This is a sample bio.</textarea>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                    <!-- Notifications Section -->
-                    <div class="tab-pane fade" id="account-notifications">
-                        <div class="cd-card-body">
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="checkbox" id="emailNotifications" checked>
-                                <label class="form-check-label" for="emailNotifications">Email Notifications</label>
-                            </div>
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" id="pushNotifications" checked>
-                                <label class="form-check-label" for="pushNotifications">Push Notifications</label>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-{{--    <div class="text-end mt-3">--}}
-{{--        <button type="button" class="btn btn-primary">Save changes</button>--}}
-{{--        <button type="button" class="btn btn-secondary">Cancel</button>--}}
-{{--    </div>--}}
+
 </div>
 @endsection
 <!-- Updated Bootstrap JS -->
